fe8b3638482b551e641b66b6a8eaa81f1a3d0dc6
AntoGrd <antoinegiroud.work@gmail.com>
2023-03-06 13:00:26 +0100

    add streamlit

---
+++ accueil_streamlit.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+def main():
+    st.header("Challenge SISE - OPSIE - 2023")
+   
+if __name__ == '__main__':
+    main()
