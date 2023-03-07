14c6012dc0e5e88dfa9303ee49a7834a915f85b9
Skarbkit <55199063+Skarbkit@users.noreply.github.com>
2023-03-07 11:06:31 +0100

    02_Predictions

---
+++ model_functions.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import plotly.express as px
+
+
+
+def to_datetime(x):
+    return pd.to_datetime(x, format='%Y-%m-%d %H:%M:%S.%f')
+
+def prepare_log_data(log_data):
+    log_data.columns = ['timestamp', 'IPsrc', 'IPdst', 'proto', 'portsrc', 'portdst', 'rule','action','7', '8','9']
+    log_data.timestamp = to_datetime(log_data.timestamp)
+    log_data.portdst = log_data.portdst.astype('object')
+    return log_data
+
+# create function of the above
+def get_IPsrc_stats(data):
+    IPsrc_deny = data[data.action == 'DENY'].groupby('IPsrc')['IPdst'].count().sort_values(ascending=False)
+    IPsrc_deny = pd.DataFrame([IPsrc_deny.index,IPsrc_deny]).transpose()
+    IPsrc_deny.columns = ['IPsrc', 'deny_count']
+    mean_timestamp_byIP = data.groupby('IPsrc').timestamp.mean().reset_index(name='mean_timestamp').sort_values(by='mean_timestamp', ascending=False)
+    IPsrc_counts = data.groupby('IPsrc').size().reset_index(name='counts').sort_values(by='counts', ascending=False)
+    return IPsrc_deny, mean_timestamp_byIP, IPsrc_counts
+
+# create function to create df_supervised with IPsrc_deny, mean_timestamp_byIP, IPsrc_counts
+
+def create_unsupervised_df(data):
+    IPsrc_deny, mean_timestamp_byIP, IPsrc_counts = get_IPsrc_stats(data)
+    ratio = pd.merge(IPsrc_counts, IPsrc_deny, on='IPsrc')
+    ratio['ratio_deny_total'] = ratio['deny_count'] / ratio['counts']
+    df_supervised = pd.merge(IPsrc_counts, mean_timestamp_byIP, on='IPsrc')
+    df_supervised = pd.merge(df_supervised, ratio[['IPsrc','ratio_deny_total']], on='IPsrc')
+
+
+    df_supervised['hour'] = df_supervised.mean_timestamp.dt.hour
+    return df_supervised
+
+def pie_pred(data):
+    res_pred = data.groupby('prediction_class').size().reset_index(name='counts')
+    # plot pie chart
+    fig = px.pie(res_pred, values="counts", names="prediction_class", color="prediction_class", title="Prediction distribution")
+    return fig
+
+def most_dangerous_ips(data):
+    
+   
--- pages/02_Predictions.py
+++ pages/02_Predictions.py
@@ -1,15 +1,68 @@
 import streamlit as st
 import pickle
+import pandas as pd
+from model_functions import *
+import plotly.express as px
+
+df = None
+df_prep = None
+
+st.title("Importez un fichier csv pour faire des prédictions")
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file, sep=';',header=None, encoding='latin-1')
+  st.write(df, df.shape)
+  
 
 st.title('Prédictions du modèle')
+
+if df is not None:
+    df_prep = create_unsupervised_df(prepare_log_data(df))
+    X_unsupervised = df_prep[['counts', 'hour', 'ratio_deny_total']]
+else : 
+    st.write('_Aucun fichier n\'a été importé_' )
 
 # load the model from disk and use cashing to speed up the loading
 @st.cache(allow_output_mutation=True)
 def load_model():
-    loaded_model = pickle.load(open('model/IsolationForest_model.pkl', 'rb'))
+    loaded_model = pickle.load(open('pages/IsolationForest_model', 'rb'))
     return loaded_model
 with st.spinner('Loading Model Into Memory...'):
     model = load_model()
 
+
+
+# make predictions
+with st.sidebar.form("Input"):
+    if df is not None:
+        btnResult = st.form_submit_button('Make Predictions', disabled=False)
+    else :
+        btnResult = st.form_submit_button('Make Predictions', disabled=True)
+    
+
+if btnResult:
+    with st.spinner('Predicting...'):
+        y_pred = model.predict(X_unsupervised)
+        att = model.decision_function(X_unsupervised)
+        df_prep['prediction_value'] = att
+        df_prep['prediction_class'] = y_pred
+        df_prep['prediction_class'] = df_prep['prediction_class'].apply(lambda x: 'Anomalie' if x == -1 else 'Normal')
+        st.write(df_prep[['IPsrc', 'counts','prediction_class', 'prediction_value']])
+
+        col1, col2 = st.columns(2)
+        with col1:
+            st.plotly_chart(pie_pred(df_prep), use_container_width=True)
+        with col2:
+            st.text('Top 10 des IPs les plus dangereuses prédites')
+            st.write(df_prep[['IPsrc', 'counts', 'prediction_value']][df_prep.prediction_class == 'Anomalie'].sort_values(by='counts', ascending=False).head(10))
+        
+
+
+
+#st.title('Anomalies détectées')
+#st.write(df_prep[df_prep.prediction_class == 'Anomalie'])
+# sort 
+   
 
 
