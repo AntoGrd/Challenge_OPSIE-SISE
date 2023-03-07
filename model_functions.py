2c3e9a38547efbc3c8908c270e6387253b46b7aa
Florian <55199063+Skarbkit@users.noreply.github.com>
2023-03-07 15:36:16 +0100

    add opsie data

--- model_functions.py
+++ model_functions.py
@@ -41,4 +41,38 @@ def pie_pred(data):
     return fig
 
     
+<<<<<<< HEAD
+# données opsie 
+
+
+def prepare_log_data_opsie(log_data):
+    log_data.columns = ['timestamp', 'IPsrc', 'IPdst', 'portdst', 'proto', 'action', 'rule']
+    log_data.timestamp = to_datetime(log_data.timestamp)
+    log_data.portdst = log_data.portdst.astype('object')
+    return log_data
+
+# create function of the above
+def get_IPsrc_stats_opsie(data):
+    IPsrc_deny = data[data.action == 'Deny'].groupby('IPsrc')['IPdst'].count().sort_values(ascending=False)
+    IPsrc_deny = pd.DataFrame([IPsrc_deny.index,IPsrc_deny]).transpose()
+    IPsrc_deny.columns = ['IPsrc', 'deny_count']
+    mean_timestamp_byIP = data.groupby('IPsrc').timestamp.mean().reset_index(name='mean_timestamp').sort_values(by='mean_timestamp', ascending=False)
+    IPsrc_counts = data.groupby('IPsrc').size().reset_index(name='counts').sort_values(by='counts', ascending=False)
+    return IPsrc_deny, mean_timestamp_byIP, IPsrc_counts
+
+# create function to create df_supervised with IPsrc_deny, mean_timestamp_byIP, IPsrc_counts
+
+def create_unsupervised_df_opsie(data):
+    IPsrc_deny, mean_timestamp_byIP, IPsrc_counts = get_IPsrc_stats_opsie(data)
+    ratio = pd.merge(IPsrc_counts, IPsrc_deny, on='IPsrc')
+    ratio['ratio_deny_total'] = ratio['deny_count'] / ratio['counts']
+    
+    df_supervised = pd.merge(IPsrc_counts, mean_timestamp_byIP, on='IPsrc')
+    df_supervised = pd.merge(df_supervised, ratio[['IPsrc','ratio_deny_total']], on='IPsrc')
+
+
+    df_supervised['hour'] = df_supervised.mean_timestamp.dt.hour
+    return df_supervised
+=======
    
+>>>>>>> 00f38019c8962a45349734bbea0aa32ba2b0862c
