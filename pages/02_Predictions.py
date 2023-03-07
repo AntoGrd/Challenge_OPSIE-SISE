8d12ece9d79daf29224731c880adf316e7cda03e
Skarbkit <55199063+Skarbkit@users.noreply.github.com>
2023-03-07 09:15:32 +0100

    load model

--- pages/02_Predictions.py
+++ pages/02_Predictions.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import pickle
 
 st.title('Prédictions du modèle')
+
+# load the model from disk and use cashing to speed up the loading
+@st.cache(allow_output_mutation=True)
+def load_model():
+    loaded_model = pickle.load(open('model/IsolationForest_model.pkl', 'rb'))
+    return loaded_model
+with st.spinner('Loading Model Into Memory...'):
+    model = load_model()
+
+
+
